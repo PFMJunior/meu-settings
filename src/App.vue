9283ad6c0e793241de1165aaafe0f8e2991edf33
Paulo Montefusco <paulo.montefusco@diasmais.com.br>
2025-10-31 14:05:57 -0300

    Remove comments

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div :class="theme === 'dark' ? 'dark' : ''">
-    <!-- Remova as classes bg-slate-50 e dark:bg-slate-900 temporariamente -->
     <div class="min-h-screen text-slate-900 dark:text-slate-100">
       <router-view />
       <DynamicDialog />
--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,7 @@ import InputSwitch from 'primevue/inputswitch'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 import { VueQueryPlugin } from '@tanstack/vue-query'
-import './index.css' // Tailwind
-
-// opcional: importa para garantir que o composable aplique styles se necessário
-// import { useTheme } from './composables/useTheme'
-// const { theme } = useTheme()
-// (não precisa fazer nada aqui; import garante efeito no módulo)
+import './index.css'
 
 const app = createApp(App)
 app.use(router)
