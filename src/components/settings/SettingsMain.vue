e4d4010a0df6d10f657fac865faf8b66348e7ff8
Paulo Montefusco <paulo.montefusco@diasmais.com.br>
2025-10-31 11:38:06 -0300

    Adding dark theme

---
+++ src/components/settings/SettingsMain.vue
@@ -0,0 +1,17 @@
+<template>
+  <div 
+    :class="{ 
+      'text-slate-900': theme === 'light', 
+      'text-slate-100': theme === 'dark' 
+    }"
+  >
+    <h2 class="text-2xl font-bold mb-4">Visão geral de configurações</h2>
+    <p>Escolha uma seção no menu à esquerda.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from '@/composables/useTheme' 
+
+const { theme } = useTheme()
+</script>
---
+++ src/components/settings/SubpageGeneric.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    :class="{ 
+      'text-slate-900': theme === 'light', 
+      'text-slate-100': theme === 'dark' 
+    }"
+  >
+    <h2 class="text-2xl font-bold mb-4">Subpágina: {{ submenu }}</h2>
+    <p>Conteúdo para a subpágina <strong>{{ submenu }}</strong></p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { useTheme } from '@/composables/useTheme' 
+
+const { theme } = useTheme()
+const route = useRoute()
+const submenu = route.params.submenu || 'home'
+</script>
