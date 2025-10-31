65d025bcfe02e63720197d248f6f106c559e0b4c
Paulo Montefusco <paulo.montefusco@diasmais.com.br>
2025-10-31 11:43:34 -0300

    Crete component

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-screen">
+    <Sidebar class="w-64 hidden md:block" />
+    <div class="flex-1">
+      <Topbar />
+      <main class="p-4">
+        <router-view />
+      </main>
+    </div>
+    <MobileSidebar />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Sidebar from '@/components/layout/Sidebar.vue'
+import Topbar from '@/components/layout/Topbar.vue'
+import MobileSidebar from '@/components/layout/MobileSidebar.vue'
+</script>
